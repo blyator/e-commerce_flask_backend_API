1a8d5feb4ee3a1fc04a7d2abdfdbae31290b2a20
Billy Yator <dmnbilly@gmail.com>
2026-02-13 15:23:19 +0300

    Implement Flask application factory pattern

--- backend/app.py
+++ backend/app.py
@@ -4,57 +4,61 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from models import db, TokenBlocklist
 from views import auth_bp, user_bp, product_bp, order_bp, category_bp, cart_bp
-import os
-from flask_cors import CORS
-from datetime import timedelta
 from views.mailserver import email
-from dotenv import load_dotenv  
-
-load_dotenv()
-
-
-
-app = Flask(__name__)
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
 
 
+jwt = JWTManager()
+migrate = Migrate()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
+def create_app():
 
+    load_dotenv()
 
-db.init_app(app)
-migrate = Migrate(app, db)
-CORS(app, credentials=True)
+    app = Flask(__name__)
 
+    # Config
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-jwt = JWTManager(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    CORS(app, credentials=True)
 
-email(app)
-
-@jwt.token_in_blocklist_loader
-def check_if_token_revoked(jwt_header, jwt_payload):
-    jti = jwt_payload['jti']
-    token = TokenBlocklist.query.filter_by(jti=jti).first()
-    return token is not None
-
-@jwt.revoked_token_loader
-def revoked_token_response(jwt_header, jwt_payload):
-    return jsonify({"error": "Token has been revoked, please login again."}), 401
-
-app.register_blueprint(auth_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(product_bp)
-app.register_blueprint(order_bp)
-app.register_blueprint(category_bp)
-app.register_blueprint(cart_bp)
+    # Initialize Mail
+    email(app)
 
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload):
+        jti = jwt_payload['jti']
+        token = TokenBlocklist.query.filter_by(jti=jti).first()
+        return token is not None
 
-@app.route('/')
-def index():
-    return {'message': 'Welcome to The shop API'}, 200
+    @jwt.revoked_token_loader
+    def revoked_token_response(jwt_header, jwt_payload):
+        return jsonify({"error": "Token has been revoked, please login again."}), 401
+
+    # Register Blueprints
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(product_bp)
+    app.register_blueprint(order_bp)
+    app.register_blueprint(category_bp)
+    app.register_blueprint(cart_bp)
+
+    @app.route('/')
+    def index():
+        return {'message': 'Welcome to The Shop API'}, 200
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
