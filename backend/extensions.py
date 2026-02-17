c9dd12aac63f9b8554970b4fd1fa6599e2441feb
Billy Yator <dmnbilly@gmail.com>
2026-02-17 15:26:17 +0300

    Flask-Limiter for API rate limiting

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,5 +1,8 @@
 from flask_caching import Cache
 from flask_mail import Mail
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 cache = Cache()
 mail = Mail()
+limiter = Limiter(key_func=get_remote_address)
